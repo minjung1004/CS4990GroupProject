c5e1d048851fb448078572448d4e988807064543
Min <minjung10402@gmail.com>
2023-01-15 11:56:07 +0900

    started ui

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,17 +1,26 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-            MLA formatter
-        </p>
+        <h1>
+            MLA Formatter
+        </h1>
       </header>
+      <body className="App-body">
+      <p>
+          Input or upload your paper here:
+        </p>
+        <input type='file' id='fileInput'></input>
+        <br></br>
+        <button id='uploadButton'>Upload</button>
+      </body>
     </div>
   );
 }
+
+
 
 export default App;
