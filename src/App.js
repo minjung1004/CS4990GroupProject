78c70df42100e217f9a482c0c6c363f82e253453
Taylor Vandenberg <taylordvandenberg@gmail.com>
2023-01-15 17:22:19 -0800

    MUI and new buttons

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import CircularProgress from '@mui/material/CircularProgress';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>
+            MLA Formatter
+        </h1>
       </header>
+      <body className="App-body">
+      <p>
+      (Sources must be formatted manually!)
+        </p>
+        <p>
+        Select a .docx file:
+        </p>
+        <Button variant="contained" component="label">
+  <input type="file" />
+</Button>
+        <br></br>
+        <p>
+        <Button variant="contained">Convert to MLA</Button>
+        </p>
+<p>
+<CircularProgress />
+</p>
+<p>
+        <Button variant="contained">Download Converted File</Button>
+        </p>
+      </body>
     </div>
   );
 }
+
+
 
 export default App;
