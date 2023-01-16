5ba5054b68ff5e63597b1489d38bbd227f196e1e
Min <minjung10402@gmail.com>
2023-01-16 10:17:00 +0900

    update

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -11,16 +11,20 @@ function App() {
       </header>
       <body className="App-body">
       <p>
-          Input or upload your paper here:
+          Upload your paper here:
         </p>
         <input type='file' id='fileInput'></input>
         <br></br>
         <button id='uploadButton'>Upload</button>
+        {/* 1. add upload function
+            2. read file from upload
+            3. change format of file to MLA
+            4. output MLA formatter file
+
+        */ }
       </body>
     </div>
   );
 }
-
-
 
 export default App;
