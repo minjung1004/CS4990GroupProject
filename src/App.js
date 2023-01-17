ad2b2c3790916f6d5d87b6d2e2732eba833b49db
timhswede <timhswede@gmail.com>
2023-01-17 14:16:25 -0800

    Add S3 Bucket upload

--- src/App.js
+++ src/App.js
@@ -7,8 +7,34 @@ import '@fontsource/roboto/700.css';
 import * as React from 'react';
 import Button from '@mui/material/Button';
 import CircularProgress from '@mui/material/CircularProgress';
+import { Upload } from "@aws-sdk/lib-storage";
+import { S3Client } from "@aws-sdk/client-s3";
+
 
 function App() {
+
+  const upload = (file)=>{
+    var file = file.target.files[0];
+
+    const target = { Bucket:"htmlformat", Key:file.name, Body:file};
+    const creds = {accessKeyId: "AKIA5I7IZWJMZMBYJEVF", secretAccessKey: "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"};
+    try {
+      const parallelUploads3 = new Upload({
+        client: new S3Client({region:"us-west-1", credentials:creds}),
+        leavePartsOnError: false,
+        params: target,
+      });
+
+      parallelUploads3.on("httpUploadProgress", (progress) => {
+        console.log(progress);
+      });
+    
+      parallelUploads3.done();
+    } catch (e) {
+      console.log(e)
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -24,16 +50,16 @@ function App() {
         Select a .docx file:
         </p>
         <Button variant="contained" component="label">
-  <input type="file" />
-</Button>
+          <input type="file" onChange={upload}></input>
+        </Button>
         <br></br>
         <p>
         <Button variant="contained">Convert to MLA</Button>
         </p>
-<p>
-<CircularProgress />
-</p>
-<p>
+        <p>
+        <CircularProgress />
+        </p>
+        <p>
         <Button variant="contained">Download Converted File</Button>
         </p>
       </body>
